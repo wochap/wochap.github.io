b79c19c15e463593ec7d32ddaa55b47a00fd28b6
wochap <gean.marroquin@gmail.com>
2016-12-04 11:31:34 -0500

    add navigation

--- src/app/components/SiteHeader.js
+++ src/app/components/SiteHeader.js
@@ -1,11 +1,40 @@
 import React from 'react'
 
-function SiteHeader () {
+function SiteHeader ({isHome}) {
   return (
-    <header className="c-site-header">
-      <h1 className="c-site-header__title">Gean Carlos Bonifacio Marroquin</h1>
+    <header className={`o-wrapper c-site-header ${isHome ? 'c-site-header--home' : ''}`}>
+      <h1 className="c-site-header__title" ><span>Gean</span> <span>Marroquin</span></h1>
+      <nav className="c-site-nav">
+        <ul>
+          <li>
+            <a className="o-btn" href="#!">About</a>
+          </li>
+          <li>
+            <a className="o-btn" href="#!">Works</a>
+          </li>
+          <li>
+            <a className="o-btn" href="#!">Blog</a>
+          </li>
+          <li>
+            <a className="o-btn" href="emailto:gean.marroquin@gmail.com">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <button
+        className="c-site-header__btn-toggle c-hamburger u-hide@laptop js-hamburger"
+        aria-label="Mostrar/Esconder navegación"
+        type="button"
+      >
+        <em />
+        <em />
+        <em />
+      </button>
     </header>
   )
+}
+
+SiteHeader.propTypes = {
+  isHome: React.PropTypes.bool
 }
 
 export default SiteHeader
