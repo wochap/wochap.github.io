21e40dffaa8c516ff8aa4b24171d167d4810bd24
wochap <gean.marroquin@gmail.com>
2016-12-12 14:56:32 -0500

    feat(SiteHeader): toggle mobile nav

--- src/app/components/SiteHeader.js
+++ src/app/components/SiteHeader.js
@@ -1,40 +1,59 @@
 import React from 'react'
+import {Link} from 'react-router'
+import Hamburger from './Hamburger'
 
-function SiteHeader ({isHome}) {
-  return (
-    <header className={`o-wrapper c-site-header ${isHome ? 'c-site-header--home' : ''}`}>
-      <h1 className="c-site-header__title" ><span>Gean</span> <span>Marroquin</span></h1>
-      <nav className="c-site-nav">
-        <ul>
-          <li>
-            <a className="o-btn" href="#!">About</a>
-          </li>
-          <li>
-            <a className="o-btn" href="#!">Works</a>
-          </li>
-          <li>
-            <a className="o-btn" href="#!">Blog</a>
-          </li>
-          <li>
-            <a className="o-btn" href="emailto:gean.marroquin@gmail.com">Contact</a>
-          </li>
-        </ul>
-      </nav>
-      <button
-        className="c-site-header__btn-toggle c-hamburger u-hide@laptop js-hamburger"
-        aria-label="Mostrar/Esconder navegación"
-        type="button"
-      >
-        <em />
-        <em />
-        <em />
-      </button>
-    </header>
-  )
-}
+class SiteHeader extends React.Component {
+  static propTypes = {
+    isHome: React.PropTypes.bool
+  }
 
-SiteHeader.propTypes = {
-  isHome: React.PropTypes.bool
+  state = {
+    isNavOpen: false
+  }
+
+  toggleNav = () => {
+    const {isNavOpen} = this.state
+
+    this.setState({
+      isNavOpen: !isNavOpen
+    })
+  }
+
+  render () {
+    const {isHome} = this.props
+    const {isNavOpen} = this.state
+
+    return (
+      <header className={`o-wrapper c-site-header ${isHome ? 'c-site-header--home' : ''}`}>
+        <h1 className="c-site-header__title" >
+          <Link className="" to="/">
+            <span>Gean</span> <span>Marroquin</span>
+          </Link>
+        </h1>
+        <nav className={`c-site-nav ${isNavOpen ? 'is-open' : ''}`}>
+          <ul>
+            <li>
+              <Link className="o-btn" to="/about">About</Link>
+            </li>
+            <li>
+              <Link className="o-btn" to="/works">Works</Link>
+            </li>
+            <li>
+              <Link className="o-btn" to="/blog">Blog</Link>
+            </li>
+            <li>
+              <a className="o-btn" href="emailto:gean.marroquin@gmail.com">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <Hamburger
+          className="c-site-header__btn-toggle u-hide@laptop"
+          onClick={this.toggleNav}
+          isOpen={isNavOpen}
+        />
+      </header>
+    )
+  }
 }
 
 export default SiteHeader
--- src/app/components/__tests__/SiteHeader.test.js
+++ src/app/components/__tests__/SiteHeader.test.js
@@ -3,15 +3,21 @@ import {shallow} from 'enzyme'
 import SiteHeader from '../SiteHeader'
 
 describe('SiteHeader', () => {
-  it('render title', () => {
-    const siteHeader = shallow(<SiteHeader />)
-
-    expect(siteHeader.find('h1').text()).toEqual('Gean Marroquin')
-  })
-
   it('add home modifier', () => {
     const siteHeader = shallow(<SiteHeader isHome />)
 
     expect(siteHeader.hasClass('c-site-header--home')).toEqual(true)
+  })
+
+  it('toggle nav on hamburger click', () => {
+    const siteHeader = shallow(<SiteHeader />)
+    const nav = siteHeader.find('.c-site-nav')
+    const hamburger = siteHeader.find('Hamburger')
+
+    hamburger.simulate('click')
+
+    expect(hamburger).toHaveLength(1)
+    expect(nav.hasClass('is-open')).toEqual(true)
+    expect(siteHeader.state().isNavOpen).toEqual(true)
   })
 })
