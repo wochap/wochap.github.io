8985c93d56315d726f04e022691a4f4af58b9e43
wochap <gean.marroquin@gmail.com>
2016-12-13 15:59:09 -0500

    add About, Works, BLog screens

--- src/app/config/routes.js
+++ src/app/config/routes.js
@@ -1,5 +1,8 @@
 import App from 'app/components/App'
 import Home from 'app/screens/Home'
+import About from 'app/screens/About'
+import Works from 'app/screens/Works'
+import Blog from 'app/screens/Blog'
 import NotFound from 'app/screens/NotFound'
 
 export default {
@@ -7,6 +10,9 @@ export default {
   component: App,
   indexRoute: Home,
   childRoutes: [
+    About,
+    Works,
+    Blog,
     NotFound
   ]
 }
