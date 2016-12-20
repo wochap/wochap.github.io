c65578b65e64454144b4f19aad0fa7e491eabbc1
wochap <gean.marroquin@gmail.com>
2016-12-20 16:42:01 -0500

    remove unnecessary isHome prop

--- src/app/components/SiteFooter.js
+++ src/app/components/SiteFooter.js
@@ -1,19 +1,11 @@
 import React from 'react'
 
-function SiteFooter ({isHome}) {
-  if (isHome) {
-    return null
-  }
-
+function SiteFooter () {
   return (
     <footer className="o-wrapper u-mt5 u-py3">
-      <p className="u-center u-m0 u-muted u-fz-xs">Design by <a href="#!"><strong>Ali avak</strong></a> and Developed by <a href="#!"><strong>Ali avak</strong></a></p>
+      <p className="u-center u-m0 u-muted u-fz-xs">Diseñado con <span className="u-fz-xl">&hearts;</span> por <a href="https://site.uplabs.com/alisaayeed" target="_blank" rel="noopener noreferrer"><strong>Ali Sayed</strong></a></p>
     </footer>
   )
-}
-
-SiteFooter.propTypes = {
-  isHome: React.PropTypes.bool
 }
 
 export default SiteFooter
