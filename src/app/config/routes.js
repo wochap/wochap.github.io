1b50c13910f1ee0ee5e23b2cf1131fb5c1f5bf0b
wochap <gean.marroquin@gmail.com>
2016-12-20 17:01:20 -0500

    scroll top on change route

--- src/app/config/routes.js
+++ src/app/config/routes.js
@@ -3,16 +3,21 @@ import Home from 'app/screens/Home'
 import About from 'app/screens/About'
 import Works from 'app/screens/Works'
 import Blog from 'app/screens/Blog'
+import Contact from 'app/screens/Contact'
 import NotFound from 'app/screens/NotFound'
 
 export default {
   path: '/',
   component: App,
+  onChange () {
+    document.getElementsByClassName('c-app__wrapper')[0].scrollIntoView()
+  },
   indexRoute: Home,
   childRoutes: [
     About,
     Works,
     Blog,
+    Contact,
     NotFound
   ]
 }
