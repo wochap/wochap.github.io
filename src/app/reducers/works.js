4c18feece25b594dd3caa7b9ed558d7c9121d8a3
wochap <gean.marroquin@gmail.com>
2016-12-26 17:40:51 -0500

    move selectors to its own folder

--- src/app/reducers/works.js
+++ src/app/reducers/works.js
@@ -38,18 +38,3 @@ export default function works (state = defaultState, action) {
     }
   }
 }
-
-export function getWorkError ({works}) { // eslint-disable-line
-  return works.error ? works.error : false
-}
-
-export function getWork ({works}, workSlug) { // eslint-disable-line
-  return {
-    ...works.data[workSlug]
-  }
-}
-
-export function getWorks ({works}) { // eslint-disable-line
-  // TODO: improve code
-  return Object.values(works.data).map(p => p)
-}
