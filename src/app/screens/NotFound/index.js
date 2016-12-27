fb604f63c1539b87161cb0666d2c269ec175709d
wochap <gean.marroquin@gmail.com>
2016-12-27 16:51:31 -0500

    unmake lazy load

--- src/app/screens/NotFound/index.js
+++ src/app/screens/NotFound/index.js
@@ -1,8 +1,6 @@
+import NotFoundScreen from './components/NotFoundScreen'
+
 export default {
   path: '*',
-  getComponent (location, cb) {
-    require.ensure([], (require) => {
-      cb(null, require('./components/NotFoundScreen').default) // eslint-disable-line
-    }, 'NotFoundScreen')
-  }
+  component: NotFoundScreen
 }
