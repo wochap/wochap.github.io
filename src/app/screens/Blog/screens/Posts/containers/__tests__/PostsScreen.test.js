8944160b255b229c2dc2424d7e0279baa2b03311
wochap <gean.marroquin@gmail.com>
2016-12-30 17:17:48 -0500

    improve tests performance

--- src/app/screens/Blog/screens/Posts/containers/__tests__/PostsScreen.test.js
+++ src/app/screens/Blog/screens/Posts/containers/__tests__/PostsScreen.test.js
@@ -3,28 +3,23 @@ import {shallow} from 'enzyme'
 import {PostsScreen} from '../PostsScreen'
 
 describe('PostsScreen', () => {
-  let props
-  let wrapper
-
-  beforeEach(() => {
-    props = {
-      posts: [
-        {
-          frontMatter: {
-            slug: 'titulo',
-            date: '21-12-2016',
-            imageUrl: 'imagen',
-            title: 'titulo',
-            description: 'descripcion'
-          }
+  const props = {
+    posts: [
+      {
+        frontMatter: {
+          slug: 'titulo',
+          date: '21-12-2016',
+          imageUrl: 'imagen',
+          title: 'titulo',
+          description: 'descripcion'
         }
-      ],
-      postsIsLoading: false,
-      postsIsFulfilled: true,
-      fetchPosts: () => {}
-    }
-    wrapper = shallow(<PostsScreen {...props} />)
-  })
+      }
+    ],
+    postsIsLoading: false,
+    postsIsFulfilled: true,
+    fetchPosts: () => {}
+  }
+  const wrapper = shallow(<PostsScreen {...props} />)
 
   it('render component', () => {
     expect(wrapper.length).toBeTruthy()
