a73f7a6263930691ace62fe17ae37681957c48a3
wochap <gean.marroquin@gmail.com>
2016-12-31 17:32:55 -0500

    replace Helmet with ScreenHelmet

--- src/app/screens/Home/components/HomeScreen.js
+++ src/app/screens/Home/components/HomeScreen.js
@@ -1,36 +1,20 @@
 import React from 'react'
 import data from 'app/config/data'
 import {Link} from 'react-router'
-import Helmet from 'react-helmet'
+import ScreenHelmet from 'components/ScreenHelmet'
 
 function HomeScreen () {
   return (
     <section className="c-home-screen">
-      <Helmet
+      <ScreenHelmet
         title={data.screens.home.title}
-        meta={[
-          {
-            name: 'description',
-            content: data.screens.home.description
-          }, {
-            property: 'og:title',
-            content: data.screens.home.title
-          }, {
-            property: 'og:description',
-            content: data.screens.home.description
-          }
-        ]}
-        link={[
-          {
-            rel: 'canonical',
-            href: data.screens.home.canonicalHref
-          }
-        ]}
+        description={data.screens.home.description}
+        canonicalHref={data.screens.home.canonicalHref}
       />
 
       <div className="o-wrapper">
         <div className="c-home-screen__wrapper">
-          <h2 className="u-caps">Hola!, soy Gean</h2>
+          <h1 className="u-caps">Hola!, soy Gean</h1>
           <p>{data.about}</p>
           <a
             className="o-btn c-btn c-btn--secondary u-mr3 u-my2"
