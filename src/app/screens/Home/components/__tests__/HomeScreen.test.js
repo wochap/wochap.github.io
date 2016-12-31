7766728e33b7bb0c0a64a476d6b18f2e0402c28e
wochap <gean.marroquin@gmail.com>
2016-12-31 17:45:37 -0500

    test if the ScreenHelmet is rendered

--- src/app/screens/Home/components/__tests__/HomeScreen.test.js
+++ src/app/screens/Home/components/__tests__/HomeScreen.test.js
@@ -3,9 +3,13 @@ import {shallow} from 'enzyme'
 import HomeScreen from '../HomeScreen'
 
 describe('HomeScreen', () => {
-  it('render component', () => {
-    const homeScreen = shallow(<HomeScreen />)
+  const homeScreen = shallow(<HomeScreen />)
 
-    expect(typeof homeScreen).toEqual('object')
+  it('render component', () => {
+    expect(homeScreen.length).toBeTruthy()
+  })
+
+  it('render ScreenHelmet', () => {
+    expect(homeScreen.find('ScreenHelmet').length).toBeTruthy()
   })
 })
