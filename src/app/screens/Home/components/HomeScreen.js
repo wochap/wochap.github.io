bc547a2bcb33a963c2f75301a5f75689ac416a14
wochap <gean.marroquin@gmail.com>
2016-12-31 17:39:12 -0500

    update HomeScreen styles

--- src/app/screens/Home/components/HomeScreen.js
+++ src/app/screens/Home/components/HomeScreen.js
@@ -12,18 +12,20 @@ function HomeScreen () {
         canonicalHref={data.screens.home.canonicalHref}
       />
 
-      <div className="o-wrapper">
-        <div className="c-home-screen__wrapper">
-          <h1 className="u-caps">Hola!, soy Gean</h1>
-          <p>{data.about}</p>
-          <a
-            className="o-btn c-btn c-btn--secondary u-mr3 u-my2"
-            href={`mailto:${data.email}`}
-          >Contáctame</a>
-          <Link
-            className="o-btn c-btn c-btn--secondary c-btn--ghost u-white u-my2"
-            to="/works"
-          >Ver portafolio</Link>
+      <div className="c-home-screen__hero">
+        <div className="o-wrapper">
+          <div className="c-home-screen__hero-wrapper">
+            <h1 className="u-caps">Hola!, soy Gean</h1>
+            <p>{data.about}</p>
+            <a
+              className="o-btn c-btn c-btn--secondary u-mr3 u-my2"
+              href={`mailto:${data.email}`}
+            >Contáctame</a>
+            <Link
+              className="o-btn c-btn c-btn--secondary c-btn--ghost u-white u-my2"
+              to="/works"
+            >Ver portafolio</Link>
+          </div>
         </div>
       </div>
     </section>
