d7de4022811993a0c65dc8a1fcd4563384d61e1c
wochap <gean.marroquin@gmail.com>
2017-01-04 14:16:06 -0500

    disable react/no-danger rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,6 +29,7 @@ module.exports = {
     'no-underscore-dangle': [0],
     'arrow-body-style': [0],
     'no-nested-ternary': [0],
+    'react/no-danger': [0],
     'max-len': [0],
     'no-console': DEBUG ? 0 : 2,
     'no-debugger': DEBUG ? 0 : 2
