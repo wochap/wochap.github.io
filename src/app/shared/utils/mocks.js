ef857999a305495e02cfa3da238b56e15a8fe1c3
wochap <gean.marroquin@gmail.com>
2017-01-16 14:14:53 -0500

    add some objects mocks

---
+++ src/app/shared/utils/mocks.js
@@ -0,0 +1,29 @@
+export const collection = [
+  {
+    frontMatter: {
+      fileName: 'fake-collection',
+      date: '2016-19-12',
+      imageUrl: 'http://placehold.it/1350x450',
+      title: 'Fake collection'
+    },
+    bodyHtml: '<p>Lorem</p>'
+  }, {
+    frontMatter: {
+      fileName: 'fake-collection-2',
+      date: '2016-20-12',
+      imageUrl: 'http://placehold.it/1350x450',
+      title: 'Fake collection 2'
+    },
+    bodyHtml: '<p>Lorem 2</p>'
+  }
+]
+
+export const collectionItem = {
+  frontMatter: {
+    fileName: 'fake-collection',
+    date: '2016-19-12',
+    imageUrl: 'http://placehold.it/1350x450',
+    title: 'Fake collection'
+  },
+  bodyHtml: '<p>Lorem</p>'
+}
