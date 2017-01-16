595d8b717ccca39674f35122a6db031c60a9ed0c
wochap <gean.marroquin@gmail.com>
2017-01-16 17:37:34 -0500

    set description optional prop

--- src/app/shared/components/ScreenHelmet.js
+++ src/app/shared/components/ScreenHelmet.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 
-function ScreenHelmet ({title, description, canonicalHref}) {
+function ScreenHelmet ({title, description = '', canonicalHref}) {
   return (
     <Helmet
       title={title}
