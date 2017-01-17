4a624995268b8b325de00e9effa1085c66fc4973
wochap <gean.marroquin@gmail.com>
2017-01-17 17:50:13 -0500

    order collection items by date

--- src/app/actions/collections.js
+++ src/app/actions/collections.js
@@ -1,4 +1,6 @@
+import moment from 'moment'
 import {normalize, Schema, arrayOf} from 'normalizr'
+import {COLLECTION_DATE_FORMAT} from 'app/config/constants'
 
 const COLLECTION_SCHEMA_NAME = 'items'
 export const FETCH_COLLECTION = 'FETCH_COLLECTION'
@@ -62,7 +64,23 @@ export function loadItemsFromCollection (collectionName) {
       collectionName
     },
     payload: require(`lazy-dir-loader!src/data/${collectionName}.config.js`)().then((items) => { // eslint-disable-line
+      items.sort(orderByDate) // eslint-disable-line
       return normalize(items, arrayOfItemsSchema).entities[COLLECTION_SCHEMA_NAME]
     })
   }
+}
+
+function orderByDate (a, b) {
+  const aDate = moment(a.frontMatter.date, COLLECTION_DATE_FORMAT).toDate()
+  const bDate = moment(b.frontMatter.date, COLLECTION_DATE_FORMAT).toDate()
+
+  if (aDate > bDate) {
+    return -1
+  }
+
+  if (aDate < bDate) {
+    return 1
+  }
+
+  return 0
 }
