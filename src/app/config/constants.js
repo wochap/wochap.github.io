516478315159c80230778f3268b6fe4dac15a568
wochap <gean.marroquin@gmail.com>
2017-01-17 17:50:30 -0500

    add global constant COLLECTION_DATE_FORMAT

---
+++ src/app/config/constants.js
@@ -0,0 +1,1 @@
+export const COLLECTION_DATE_FORMAT = 'DD-MM-YYYY' // eslint-disable-line
--- src/app/shared/utils/formatter.js
+++ src/app/shared/utils/formatter.js
@@ -1,10 +1,15 @@
 import moment from 'moment'
+import {COLLECTION_DATE_FORMAT} from 'app/config/constants'
 
 /**
  * format date with moment
  * @param  {String} date - the date in 'DD-MM-YYYY' format
  * @return {String}      - the date formatted. e.g.: 21 de diciembre de 2016
  */
-export function formatDate (date) { // eslint-disable-line
-  return moment(date, 'YYYY-MM-DD').format('LL')
+export function formatDate (date, hideDay = false) { // eslint-disable-line
+  if (hideDay) {
+    return moment(date, COLLECTION_DATE_FORMAT).format('MMMM YYYY')
+  }
+
+  return moment(date, COLLECTION_DATE_FORMAT).format('LL')
 }
