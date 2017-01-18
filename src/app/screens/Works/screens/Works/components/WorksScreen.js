ee1c990f98b82d980111783c6ae8062d90283561
wochap <gean.marroquin@gmail.com>
2017-01-18 12:10:08 -0500

    update pending state WorksScreen
    other changes:
    * add ShimmerText block modifier

--- src/app/screens/Works/screens/Works/components/WorksScreen.js
+++ src/app/screens/Works/screens/Works/components/WorksScreen.js
@@ -3,11 +3,24 @@ import data from 'app/config/data'
 import withCollection from 'hoc/collection/withCollection'
 import * as sharedPropTypes from 'hoc/collection/sharedPropTypes'
 import ScreenHelmet from 'components/ScreenHelmet'
+import ShimmerText from 'components/ShimmerText'
 import WorksList from './WorksList'
 
 export function WorksScreen ({collectionState, collection}) {
   const body = collectionState.error ? <p>Error: {collectionState.error}</p> : (
-    collectionState.isPending ? <p>Cargando trabajos...</p> : (
+    collectionState.isPending ? (
+      <ShimmerText block="true">
+        <ul className="o-grid o-grid--gutter-3 u-list-reset">
+          {[...new Array(3)].map(() => (
+            <li className="o-grid__item u-12/12 u-6/12@tablet u-4/12@laptop">
+              <div className="c-work u-relative u-overflow-hidden">
+                <span className="u-absolute u-left-0 u-top-0" style={{fontSize: '20rem'}} aria-label="Cargando">██</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </ShimmerText>
+    ) : (
       <WorksList works={collection} />
     )
   )
--- src/app/shared/components/ShimmerText.js
+++ src/app/shared/components/ShimmerText.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import classNames from 'classnames'
 
-function ShimmerText ({darken = false, text, children}) {
+function ShimmerText ({darken = false, block = false, text, children}) {
   return (
-    <div className={`c-shimmer-text ${darken ? 'c-shimmer-text--darken' : null}`}>
+    <div
+      className={classNames('c-shimmer-text', {
+        'c-shimmer-text--darken': darken,
+        'c-shimmer-text--block': block
+      })}
+    >
       <div className="c-shimmer-text__text u-blokk-font">{text || children}</div>
     </div>
   )
@@ -10,6 +16,7 @@ function ShimmerText ({darken = false, text, children}) {
 
 ShimmerText.propTypes = {
   darken: React.PropTypes.bool,
+  block: React.PropTypes.bool,
   text: React.PropTypes.string,
   children: React.PropTypes.node
 }
