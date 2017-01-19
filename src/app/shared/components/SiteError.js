6210a1adac4ac12b596bee13c3e323efdfacd2cc
wochap <gean.marroquin@gmail.com>
2017-01-19 08:07:48 -0500

    add invert modifier

--- src/app/shared/components/SiteError.js
+++ src/app/shared/components/SiteError.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 
-function SiteError ({headTitle, title, message}) {
+function SiteError ({headTitle, title, message, invert}) {
   return (
-    <div className="c-site-error">
+    <div className={`c-site-error ${invert ? 'c-site-error--invert' : ''}`}>
       {headTitle && <Helmet title={headTitle} />}
       <h1 className="c-site-error__title" data-text={title}>{title}</h1>
       <p className="c-site-error__message u-caps">{message}</p>
@@ -14,7 +14,8 @@ function SiteError ({headTitle, title, message}) {
 SiteError.propTypes = {
   headTitle: React.PropTypes.string.isRequired,
   title: React.PropTypes.string.isRequired,
-  message: React.PropTypes.string.isRequired
+  message: React.PropTypes.string.isRequired,
+  invert: React.PropTypes.bool
 }
 
 export default SiteError
