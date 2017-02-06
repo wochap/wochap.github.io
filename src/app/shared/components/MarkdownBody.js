6ebd43c790380210d1e1304bcc7c5c41a07b3285
wochap <gean.marroquin@gmail.com>
2017-02-06 18:28:36 -0500

    update ShimmerText content

--- src/app/shared/components/MarkdownBody.js
+++ src/app/shared/components/MarkdownBody.js
@@ -4,10 +4,11 @@ import ShimmerText from 'components/ShimmerText'
 function MarkdownBody ({html, isLoading = false}) {
   if (isLoading) {
     return (
-      <div className="o-wrapper o-wrapper--3">
+      <div className="o-wrapper o-wrapper--3 u-pb5">
         <ShimmerText>
-          <span className="u-block u-pb3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span>
-          <span>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+          <span className="u-block u-pb3 u-fz-h00 u-line-height-1" aria-label="cargando">Lorem</span>
+          <span className="u-block u-pb3" aria-label="cargando">Loremipsumdolorsitamet,consecteturadipisicingelit,seddoeiusmodtemporincididuntutlaboreetdoloremagnaaliqua.Utenimadminimveniam,quisnostrudexercitationullamcolaborisnisiutaliquipexeacommodoconsequat.</span>
+          <span aria-label="cargando">Duisauteiruredolorinreprehenderitinvoluptatevelitessecillumdoloreeufugiatnullapariatur.Excepteursintoccaecatcupidatatnonproident,suntinculpaquiofficiadeseruntmollitanimidestlaborum.</span>
         </ShimmerText>
       </div>
     )
