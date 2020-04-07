6ebd61621d9b1ff30746a23cca563ee23b12567d
Gean <gean.marroquin@gmail.com>
2020-04-07 16:26:32 -0500

    Updates

--- static/js/chunk.BlogScreen.268a7e3f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{543:function(n,e,c){"use strict";c.r(e);var l=c(1),o=c.n(l),p=c(0),r=c.n(p);function t({children:n}){return n?o.a.cloneElement(n):null}t.propTypes={children:r.a.element},e.default=t}}]);
