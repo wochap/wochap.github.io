614c122571cf3818a7f5dfa62151da076273df84
Gean <gean.marroquin@gmail.com>
2020-04-08 16:05:16 -0500

    refactor: babel config
    add '@babel/plugin-proposal-optional-chaining', '@babel/plugin-proposal-nullish-coalescing-operator'

---
+++ babel.config.js
@@ -0,0 +1,47 @@
+const presets = [
+  [
+    '@babel/preset-env',
+    {
+      modules: false,
+    },
+  ],
+  '@babel/preset-react',
+  '@babel/preset-flow',
+]
+const commonPlugins = [
+  '@babel/plugin-proposal-class-properties',
+  '@babel/plugin-proposal-export-namespace-from',
+  '@babel/plugin-proposal-export-default-from',
+  '@babel/plugin-proposal-optional-chaining',
+  '@babel/plugin-proposal-nullish-coalescing-operator',
+]
+const clientPlugins = [
+  ...commonPlugins,
+  [
+    '@babel/plugin-transform-runtime',
+    {
+      corejs: 3,
+    },
+  ],
+]
+
+module.exports = {
+  env: {
+    test: {
+      presets,
+      plugins: [...commonPlugins, 'transform-es2015-modules-commonjs', 'dynamic-import-node'],
+    },
+    ssr: {
+      presets,
+      plugins: clientPlugins,
+    },
+    development: {
+      presets,
+      plugins: clientPlugins,
+    },
+    production: {
+      presets,
+      plugins: clientPlugins,
+    },
+  },
+}
