fb9d5c37e0597b3eca1f70f9875c4415c81f77aa
Gean <gean.marroquin@gmail.com>
2020-04-11 12:51:21 -0500

    feat: show readingTime in blog posts

--- src/app/shared/components/Post.jsx
+++ src/app/shared/components/Post.jsx
@@ -6,7 +6,11 @@ import {formatDate} from 'utils/formatter'
 function Post({post}) {
   return (
     <article>
-      <p className='u-fz-sm u-mb1 u-muted'>{formatDate(post.date)}</p>
+      <p className='u-fz-sm u-mb1 u-muted'>
+        {formatDate(post.date)}
+        <br />
+        <small>{post.readingTime}</small>
+      </p>
       <h3 className='u-fz-h2 u-mb0 u-line-height-1'>
         <Link to={`/blog/${post.fileName}`}>{post.title}</Link>
       </h3>
@@ -16,6 +20,7 @@ function Post({post}) {
 
 Post.propTypes = {
   post: PropTypes.shape({
+    readingTime: PropTypes.string.isRequired,
     fileName: PropTypes.string.isRequired,
     date: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
