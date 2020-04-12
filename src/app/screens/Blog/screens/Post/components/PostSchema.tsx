41eb3983582f2037459dca52a08fb2be2262e01c
Gean <gean.marroquin@gmail.com>
2020-04-11 21:53:10 -0500

    feat: add schema.org BlogPosting for blog posts
    add PostSchema.tsx,

---
+++ src/app/screens/Blog/screens/Post/components/PostSchema.tsx
@@ -0,0 +1,55 @@
+// TODO: apply i18n
+import moment from 'moment'
+import React from 'react'
+import {BlogPosting} from 'schema-dts'
+import {JsonLd} from 'react-schemaorg'
+import data from 'app/config/data'
+import {articleShape} from 'app/shared/PropTypes/article'
+import {COLLECTION_DATE_FORMAT} from 'app/config/constants'
+
+function PostSchema({item}) {
+  if (!item?.state?.isFulfilled) {
+    return null
+  }
+
+  const {frontMatter} = item
+  const datePublished = moment(frontMatter.date, COLLECTION_DATE_FORMAT).format()
+  const dateModified = frontMatter.dateModified ? moment(frontMatter.dateModified, COLLECTION_DATE_FORMAT).format() : datePublished
+
+  // TODO: add 'articleBody'
+  return (
+    <JsonLd<BlogPosting>
+      item={{
+        '@context': 'https://schema.org',
+        '@type': 'BlogPosting',
+        'author': {
+          '@type': 'Person',
+          'name': data.title,
+        },
+        'datePublished': datePublished,
+        'headline': frontMatter.title,
+        'image': frontMatter.imageUrl,
+        'publisher': {
+          '@type': 'Person',
+          'name': data.title,
+        },
+        'dateModified': dateModified,
+        'mainEntityOfPage': {
+          '@type': 'URL',
+          'category': `${data.screens.blog.canonicalHref}/${frontMatter?.slug}`,
+        },
+        'keywords': frontMatter.tags.join(' '),
+        'inLanguage': {
+          '@type': 'Language',
+          'name': 'Spanish',
+          'alternateName': 'es',
+        },
+      }}
+    />
+  )
+}
+PostSchema.propTypes = {
+  item: articleShape.isRequired,
+}
+
+export default PostSchema
