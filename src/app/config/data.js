b5acfde13211c9705362eed9c242730028991d3a
Gean <gean.marroquin@gmail.com>
2020-04-12 09:36:37 -0500

    chore: update data for better SEO?

--- src/app/config/data.js
+++ src/app/config/data.js
@@ -3,44 +3,46 @@ import prevalMarkdown from '../../../lib/macros/markdown.macro'
 const DOMAIN_NAME = 'geanmarroquin.com'
 const BASE_URL = `https://${DOMAIN_NAME}`
 const TITLE = 'Gean Marroquin'
-const ABOUT = prevalMarkdown`
-  Establecido en Perú, soy <i class="u-icon u-color-javascript u-fz-h2">javascript</i> Developer enfocado en la creación de componentes escalables y reusables con <i class="u-icon u-color-vue_js u-fz-h2">vue_js</i> y <i class="u-icon u-color-react u-fz-h2">react</i>, apasionado y autodidacta desde el principio.
+const ABOUT_HTML = prevalMarkdown`
+  Establecido en Perú, soy <i class="u-icon u-color-javascript u-fz-h2">javascript</i> Developer enfocado en la creación de componentes escalables y reusables con <i class="u-icon u-color-vue_js u-fz-h2">vue_js</i> y <i class="u-icon u-color-react u-fz-h2">react</i>, apasionado y autodidacta.
 `
+const ABOUT =
+  'Establecido en Peru, soy Javascript Developer enfocado en la creación de componentes escalables y reusables con vue y react, apasionado y autodidacta.'
 
 export default {
   domainName: DOMAIN_NAME,
   baseUrl: BASE_URL,
   title: TITLE,
   email: 'gean.marroquin@gmail.com',
-  about: ABOUT,
+  about: ABOUT_HTML,
   twitterUser: 'geanmarroquin',
   screens: {
     about: {
-      title: `${TITLE} - JS Developer`,
+      title: `JS Developer | ${TITLE}`,
       description: ABOUT,
       canonicalHref: `${BASE_URL}/about`,
     },
     blog: {
-      title: `${TITLE} - Artículos y tutoriales`,
-      description: `Blog de ${TITLE}.`,
+      title: `Artículos y tutoriales | ${TITLE}`,
+      description: `Tutoriales sobre desarrollo de aplicaciones web y mobile.`,
       canonicalHref: `${BASE_URL}/blog`,
     },
     contact: {
-      title: `${TITLE} - Contacto`,
+      title: `Contacto | ${TITLE}`,
       description: ABOUT,
       canonicalHref: `${BASE_URL}/contact`,
     },
     home: {
-      title: `${TITLE} - Inicio`,
+      title: `Inicio | ${TITLE}`,
       description: ABOUT,
       canonicalHref: BASE_URL,
     },
     notFound: {
-      title: `${TITLE} - Página no encontrada`,
+      title: `Página no encontrada | ${TITLE}`,
     },
     works: {
-      title: `${TITLE} - Portafolio`,
-      description: `Portafolio de ${TITLE}.`,
+      title: `Portafolio | ${TITLE}`,
+      description: `Portafolio de ${TITLE}, proyectos como freelance y experimentos personales.`,
       canonicalHref: `${BASE_URL}/works`,
     },
   },
