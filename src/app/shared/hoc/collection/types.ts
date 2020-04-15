b55c4090092f160546abbf5e4a717f98700abe2b
Gean <gean.marroquin@gmail.com>
2020-04-15 14:51:03 -0500

    feat: add keywords field

--- src/app/shared/hoc/collection/types.ts
+++ src/app/shared/hoc/collection/types.ts
@@ -4,17 +4,20 @@ export interface State {
   error: boolean | Error
 }
 
+export interface FrontMatter {
+  slug: string
+  title: string
+  description: string
+  date: string
+  dateModified: string
+  imageUrl: string
+  imageCaption: string
+  tags: string[]
+  keywords?: string[]
+}
+
 export interface CollectionItem {
-  frontMatter: {
-    slug: string
-    title: string
-    description: string
-    date: string
-    dateModified: string
-    imageUrl: string
-    imageCaption: string
-    tags: string[]
-  }
+  frontMatter: FrontMatter
   bodyHtml: string
   state: State
 }
