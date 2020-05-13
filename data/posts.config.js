d66a4639bc32bc1798591adef9a9f409f1abf894
Gean <gean.marroquin@gmail.com>
2020-05-13 17:58:04 -0500

    feat: enable draft posts

--- data/posts.config.js
+++ data/posts.config.js
@@ -1,6 +1,8 @@
+const contextFilter = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'ssr' ? /^(?!.*\.draft\.md$).*\.md$/ : /\.md$/
+
 module.exports = {
   chunkName: 'posts',
   folderPath: 'data/posts',
-  contextFilter: /\.md$/,
+  contextFilter,
   loaders: ['front-matter-loader'],
 }
--- data/works.config.js
+++ data/works.config.js
@@ -1,6 +1,8 @@
+const contextFilter = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'ssr' ? /^(?!.*\.draft\.md$).*\.md$/ : /\.md$/
+
 module.exports = {
   chunkName: 'works',
   folderPath: 'data/works',
-  contextFilter: /\.md$/,
+  contextFilter,
   loaders: ['front-matter-loader'],
 }
