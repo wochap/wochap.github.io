030b545f9f0a22fe562d229c7fef00fcf91a849f
Gean <gean.marroquin@gmail.com>
2020-05-15 10:40:09 -0500

    fix: async imports of md files
    support draft and i18n md files

--- data/posts.config.js
+++ data/posts.config.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line
-const getAllExceptDraft = new RegExp('^(?!.*\.draft\.' + process.env.SSG_LANG + '\.md$).*\.' + process.env.SSG_LANG + '\.md$', 'i')
+const getAllExceptDraft = new RegExp('^(?!.*.draft.' + process.env.SSG_LANG + '.md$).*.' + process.env.SSG_LANG + '.md$', 'i')
 // eslint-disable-next-line
-const getAll = new RegExp('\.' + process.env.SSG_LANG + '\.md$', 'i')
+const getAll = new RegExp('(\\.draft)?' + '\\.' + process.env.SSG_LANG + '\\.md$', 'i')
 const contextFilter = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'ssr' ? getAllExceptDraft : getAll
 
 module.exports = {
